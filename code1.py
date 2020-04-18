1c1294374d8a19fe9e6419ecc9e3e6c04918ea25
Gaurav G Pachpande <gaurav.1116@outlook.com>
2020-04-18 23:30:31 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,6 @@
+example_1= 123
+example_2= 2+3*(2**3)
+example_3= "hellogaurav"
+print(example_1)
+print(example_2)
+print(example_3)
---
+++ code2.py
@@ -0,0 +1,4 @@
+example=68.89/89.97
+print(round(example,2))
+print(round(example,3))
+print(round(example,1))
